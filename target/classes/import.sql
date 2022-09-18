077a5f44689bd3e7fa7ed4e6b24e8f0df06027f1
Sonu Sangwan <sonusangwan.asiwas@gmail.com>
2022-09-18 21:32:39 +0530

    Added jar file to project

---
+++ target/classes/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO roles  VALUES (1,'ROLE_USER');
+INSERT INTO roles  VALUES (2,'ROLE_ADMIN');
+INSERT INTO privilege VALUES (1,'READ');
+INSERT INTO privilege VALUES (2,'WRITE');
+INSERT INTO privilege VALUES (3,'UPDATE');
+INSERT INTO privilege VALUES (4,'DELETE');
+INSERT INTO roles_privileges VALUES (1,1);
+INSERT INTO roles_privileges VALUES (2,1);
+INSERT INTO roles_privileges VALUES (2,2);
+INSERT INTO roles_privileges VALUES (2,3);
+INSERT INTO roles_privileges VALUES (2,4);
+
+
+
